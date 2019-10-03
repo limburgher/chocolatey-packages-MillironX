e906ec982adc0102ba531c84bb2a938bcb805c97
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2019-10-02 20:26:53 -0700

    Add packages for e-sword and trelby

---
+++ trelby/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'Setup-Trelby-2.2.exe'
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  file         = $fileLocation
+
+  softwareName  = 'Trelby' 
+  checksum      = '1D8A55160B2814D76672D9FD45A07D00236CEC242550B068B2BB558B48E217B'
+  checksumType  = 'sha256'
+  silentArgs   = '/S'           # NSIS
+}
+
+Install-ChocolateyInstallPackage @packageArgs
