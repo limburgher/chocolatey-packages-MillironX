bff67d5322b415fbda0f50340ed04645ca4d407a
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2019-11-16 22:13:16 -0800

    Change triplea name and version

---
+++ triplea/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation   = Join-Path $toolsDir 'TripleA_1.9.0.0.13066_windows-32bit.exe'
+$fileLocation64 = Join-Path $toolsDir 'TripleA_1.9.0.0.13066_windows-64bit.exe'
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  file          = $fileLocation
+  file64        = $fileLocation64
+  softwareName  = 'TripleA*'
+  checksum      = '98698a6fb02dca79ce6e1e2427d7e847'
+  checksumType  = 'md5'
+  checksum64    = 'ed5aaf4a012adeb03aeb282a222880b7'
+  checksumType64= 'md5'
+  silentArgs   = '-q'           # Install4j
+}
+
+Install-ChocolateyInstallPackage @packageArgs
