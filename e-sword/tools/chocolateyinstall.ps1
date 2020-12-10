7cc2def612391a1bb36ad12f57add4d958fcb470
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2020-12-10 13:48:32 -0700

    Update package specs for e-sword 12.2.0

--- e-sword/tools/chocolateyinstall.ps1
+++ e-sword/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://e-sword.net/files/e-sword_1210_setup.exe' # download url, HTTPS preferred
+$url        = 'https://e-sword.net/files/e-sword_1220_setup.exe' # download url, HTTPS preferred
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -8,7 +8,7 @@ $packageArgs = @{
   fileType      = 'EXE'
   url           = $url
   softwareName  = 'e-Sword'
-  checksum      = '3D3E01C1F289D4E6F4B83D1416650CCF572B615BA799CC9764D3B6259E6CEBBF'
+  checksum      = '26aa2ae3342ae214b4598d031f1cd0c3d237bec1fb69419349c0560532e9769d'
   checksumType  = 'sha256'
   validExitCodes= @(0, 3010, 1641)
   silentArgs   = '/s /v"/qn"'
