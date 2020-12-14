45ae858a92bea25424882aeff1b91fbf92dc5e3a
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2020-12-14 11:11:57 -0700

    Update TripleA files to 2.5.22294

--- triplea/tools/chocolateyinstall.ps1
+++ triplea/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileLocation   = Join-Path $toolsDir 'TripleA_1.9.0.0.13066_windows-32bit.exe'
-$fileLocation64 = Join-Path $toolsDir 'TripleA_1.9.0.0.13066_windows-64bit.exe'
+$fileLocation   = Join-Path $toolsDir 'TripleA_2.5.22294_windows-32bit.exe'
+$fileLocation64 = Join-Path $toolsDir 'TripleA_2.5.22294_windows-64bit.exe'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
@@ -9,10 +9,10 @@ $packageArgs = @{
   file          = $fileLocation
   file64        = $fileLocation64
   softwareName  = 'TripleA*'
-  checksum      = '98698a6fb02dca79ce6e1e2427d7e847'
-  checksumType  = 'md5'
-  checksum64    = 'ed5aaf4a012adeb03aeb282a222880b7'
-  checksumType64= 'md5'
+  checksum      = '1b8c3d87f6c16b0c1b4bdbe415972b9be4846a44f0dc89d46fab005dfd02bedd'
+  checksumType  = 'sha256'
+  checksum64    = '0eddc4e13bff3a1bfcad6a1d5f2429557552076d0c70f5fcd1aaf8f1cf889dea'
+  checksumType64= 'sha256'
   silentArgs   = '-q'           # Install4j
 }
 
