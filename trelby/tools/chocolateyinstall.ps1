46b0d614195079e64861f7f456f3b70b2d605c4f
Gwyn Ciesla <gwync@protonmail.com>
2025-08-19 16:32:45 -0500

    Update to latest trelby release.

--- trelby/tools/chocolateyinstall.ps1
+++ trelby/tools/chocolateyinstall.ps1
@@ -1,16 +1,2 @@
-﻿$ErrorActionPreference = 'Stop'; # stop on all errors
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileLocation = Join-Path $toolsDir 'Setup-Trelby-2.2.exe'
-$packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  fileType      = 'EXE'
-  file         = $fileLocation
-
-  softwareName  = 'Trelby*'
-  checksum      = '1D8A55160B2814D76672D9FD45A07D00236CEC242550B068B2BB558B48E217B'
-  checksumType  = 'sha256'
-  silentArgs   = '/S'           # NSIS
-}
-
-Install-ChocolateyInstallPackage @packageArgs
+﻿Start-ChocolateyProcessAsAdmin "pip3 install trelby"
+Install-ChocolateyShortcut -ShortcutFilePath "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\trelby.lnk" -TargetPath "C:\Python313\Scripts\trelby.exe" -IconLocation "C:\Python313\DLLs\py.ico"
